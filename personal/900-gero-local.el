e216b29f29a4e95fc64693ecfd3676a2df1504cf
DarkThrone <garciasgritta@gmail.com>
2016-12-17 12:56:58 +1100

    Fix for linum gigantic font-size

--- personal/900-gero-local.el
+++ personal/900-gero-local.el
@@ -9,6 +9,19 @@
 (setq default-frame-alist '((font . "Meslo LG M DZ-13")))
 (setq prelude-whitespace nil)
 
+;fix for linum mode
+(require 'linum)
+(defun linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (set-window-margins win
+                      (ceiling (* (if (boundp 'text-scale-mode-step)
+                                      (expt text-scale-mode-step
+                                            text-scale-mode-amount) 1)
+                                  (if (car (window-margins))
+                                      (car (window-margins)) 1)
+                                  ))))
+(advice-add #'linum-update-window :after #'linum-update-window-scale-fix)
+
 (global-linum-mode 1)
 
 (find-file (concat user-home "/.emacs.d/personal/900-gero-local.el"))
